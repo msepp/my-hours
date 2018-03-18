ca5b8fe0fb870698847765379726e91e2674a200
Mikko Seppänen <mikko.o.seppanen@gmail.com>
2018-03-18 14:47:28 +0200

    Updated to use hash location strategy to make things easier with AWS and fixed manifest for serving from root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [
     AppInitResolver
